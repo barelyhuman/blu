572bb265f149222285b30662cbb247accfa60cad
Reaper Gelera <sidgroks@gmail.com>
2021-03-10 13:43:35 +0530

    fix: add icon path to build script

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 rm -rf blu blu.app
-go build 
-fyne package -os darwin 
+go build
+fyne package -os darwin -icon Icon.png
 mkdir -p blu.app/Contents/Resources/scripts
 cp ./scripts/* blu.app/Contents/Resources/scripts
 
